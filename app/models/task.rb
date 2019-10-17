42ee72dfdbda77e7c94fc4af1396739ce5ae582b
SG24 <shreyanshgupta98@gmail.com>
2019-10-17 11:46:35 +0530

    adds before validation to add default task description if omitted by the user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
+  before_validation :assign_description, unless: :description_present
+
   validates :description, presence: true
+
+  def assign_description
+    self.description = 'No task description provided!'
+  end
+
+  def description_present
+    description.present?
+  end
 end
